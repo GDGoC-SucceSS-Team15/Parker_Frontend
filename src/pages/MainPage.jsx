550da0ec02657d225e1b40572ea876a0eef97bd4
solux <lee.yeji0327@gmail.com>
2025-02-02 21:31:02 +0900

    add: mainpage - 신고 모달 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import CustomModal from "../components/Modals/CustomModal";
+import ReportContent from "../components/Modals/ReportContent";
 
 function MainPage() {
-  return <div>메인페이지</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div>
+      <div onClick={() => setModalOpen(true)}>메인페이지</div>
+      <CustomModal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <ReportContent currentLocation="서울 강남구 역삼로 133" />
+      </CustomModal>
+    </div>
+  );
 }
 
 export default MainPage;
