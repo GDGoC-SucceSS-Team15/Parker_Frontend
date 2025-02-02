095f26fbf20b6713cee31d99b3a66c8c93b8a4ae
solux <lee.yeji0327@gmail.com>
2025-02-02 21:31:45 +0900

    del: 충돌 방지를 위해 mainpage 내용 삭제

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,20 +1,7 @@
-import React, { useState } from "react";
-import CustomModal from "../components/Modals/CustomModal";
-import ReportContent from "../components/Modals/ReportContent";
+import React from "react";
 
 function MainPage() {
-  const [modalOpen, setModalOpen] = useState(false);
-  return (
-    <div>
-      <div onClick={() => setModalOpen(true)}>메인페이지</div>
-      <CustomModal
-        isOpen={modalOpen}
-        onRequestClose={() => setModalOpen(false)}
-      >
-        <ReportContent currentLocation="서울 강남구 역삼로 133" />
-      </CustomModal>
-    </div>
-  );
+  return <div>메인페이지</div>;
 }
 
 export default MainPage;
