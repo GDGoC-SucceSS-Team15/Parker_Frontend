b49dd289334e63adc73fa7eab45b948c974f044f
solux <lee.yeji0327@gmail.com>
2025-02-03 16:29:11 +0900

    feat: BottomBar ReportButton - 신고 모달 연결

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,35 +1,43 @@
 import { useNavigate } from "react-router-dom";
-import Parker_Logo from "./../assets/LogoImage.svg"
-import MyPage from "./../assets/Bottom_Button/BT_MyPage.svg"
-import SavedParkingSpaces from "./../assets/Bottom_Button/BT_SavedParkingSpaces.svg"
-import ParkingSpaces from "./../assets/Bottom_Button/BT_ParkingSpaces.svg"
-import ParkInfo from "./../assets/Bottom_Button/BT_ParkInfo.svg"
-import Report from "./../assets/ReportButton.svg"
+import Parker_Logo from "./../assets/LogoImage.svg";
+import MyPage from "./../assets/Bottom_Button/BT_MyPage.svg";
+import SavedParkingSpaces from "./../assets/Bottom_Button/BT_SavedParkingSpaces.svg";
+import ParkingSpaces from "./../assets/Bottom_Button/BT_ParkingSpaces.svg";
+import ParkInfo from "./../assets/Bottom_Button/BT_ParkInfo.svg";
+import Report from "./../assets/ReportButton.svg";
 import { MdMyLocation } from "react-icons/md";
 import { styled } from "styled-components";
+import { useState } from "react";
+import CustomModal from "./Modals/CustomModal";
+import ReportContent from "./Modals/ReportContent";
 
-const BottomBar = ({ onGetLocation }) => { 
+const BottomBar = ({ onGetLocation }) => {
   const navigate = useNavigate();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
 
   return (
     <div>
       <BottombarDiv>
         <LeftGroup>
-          <button onClick={() => navigate("/mypage")} >
+          <button onClick={() => navigate("/mypage")}>
             <img src={MyPage} alt="MyPage" />
           </button>
           <button className="saved-parking-spaces">
             <img src={SavedParkingSpaces} alt="SavedParkingSpaces" />
           </button>
         </LeftGroup>
-        <MainButton onClick={() => navigate("/")} >
+        <MainButton onClick={() => navigate("/")}>
           <img src={Parker_Logo} alt="Parker_Logo" />
         </MainButton>
         <RightGroup>
-          <button onClick={() => navigate("/parking-spaces")} >
+          <button onClick={() => navigate("/parking-spaces")}>
             <img src={ParkingSpaces} alt="ParkingSpaces" />
           </button>
-          <button onClick={() => navigate("/parkinginfo")} >
+          <button onClick={() => navigate("/parkinginfo")}>
             <img src={ParkInfo} alt="ParkInforch_Button" />
           </button>
         </RightGroup>
@@ -37,12 +45,19 @@ const BottomBar = ({ onGetLocation }) => {
       <LocationButton onClick={onGetLocation}>
         <MdMyLocation color="#636363" size={30} />
       </LocationButton>
-      <ReportButton>
+      <ReportButton onClick={handleModalOpen}>
         <img src={Report} alt="Report" />
       </ReportButton>
+
+      <CustomModal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <ReportContent currentLocation="현재위치" />
+      </CustomModal>
     </div>
   );
-}
+};
 
 export default BottomBar;
 
@@ -121,9 +136,9 @@ const MainButton = styled.button`
   position: absolute;
   background-color: #ffffff;
   border: none;
-  width: 85px;  
+  width: 85px;
   height: 85px;
-  top: -33px; 
+  top: -33px;
   left: 50%;
   transform: translateX(-50%);
   padding: 10px;
@@ -147,10 +162,10 @@ const LocationButton = styled.button`
   border: none;
   padding: 0;
   justify-content: center;
-  align-items: center; 
-  border-radius: 50%;  
+  align-items: center;
+  border-radius: 50%;
   left: 8%;
-  width: 42px;  
+  width: 42px;
   height: 42px;
   bottom: 95px;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
@@ -162,16 +177,17 @@ const ReportButton = styled.button`
   position: absolute;
   border: none;
   justify-content: center;
-  align-items: center; 
-  border-radius: 50%;  
+  align-items: center;
+  border-radius: 50%;
   right: 8%;
-  width: 50px;  
+  width: 50px;
   height: 50px;
   bottom: 95px;
   z-index: 5;
+  cursor: pointer;
 
   img {
     width: 85px;
-    height: 85px%;
+    height: 85px;
   }
 `;
