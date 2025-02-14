3d537519eacbbb3a26a197ae3d98c00f1acc9199
solux <lee.yeji0327@gmail.com>
2025-02-14 22:35:22 +0900

    add: 단속 확률 버튼에 모달 연결

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,17 +1,20 @@
 import Menu from "./Menu";
 import { IoSearchSharp } from "react-icons/io5";
 import { FiMenu } from "react-icons/fi";
-import Parking from "./../assets/ParkingIcon.svg"
-import Warning from "./../assets/WarningIcon.svg"
-import Fire from "./../assets/FireIcon.svg"
+import Parking from "./../assets/ParkingIcon.svg";
+import Warning from "./../assets/WarningIcon.svg";
+import Fire from "./../assets/FireIcon.svg";
 import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import CustomModal from "./Modals/CustomModal";
+import UploadModal from "./Ai/UploadModal";
 
 const TopBar = ({ onSearch, onToggle }) => {
   const [input, setInput] = useState("");
   const [isMenuVisible, setIsMenuVisible] = useState(false);
-  const menuRef = useRef(null); 
-  const topBarRef = useRef(null); 
+  const menuRef = useRef(null);
+  const topBarRef = useRef(null);
+  const [openModal, setOpenModal] = useState(false);
 
   // 검색어를 부모 컴포넌트로 전달
   const handleChange = (e) => {
@@ -45,8 +48,10 @@ const TopBar = ({ onSearch, onToggle }) => {
     const handleClickOutside = (event) => {
       // 메뉴 외부를 클릭한 경우
       if (
-        menuRef.current && !menuRef.current.contains(event.target) && 
-        topBarRef.current && !topBarRef.current.contains(event.target)
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        topBarRef.current &&
+        !topBarRef.current.contains(event.target)
       ) {
         closeMenu();
       }
@@ -66,15 +71,15 @@ const TopBar = ({ onSearch, onToggle }) => {
         <MenuButton onClick={toggleMenu}>
           <FiMenu color="#636363" size={20} />
         </MenuButton>
-        <SearchInput 
+        <SearchInput
           type="text"
           placeholder="검색어를 입력하세요."
           value={input}
           onChange={handleChange}
-          onKeyPress={handleKeyPress} 
+          onKeyPress={handleKeyPress}
         />
-        <SearchButton onClick={handleSearch} >
-          <IoSearchSharp color="#636363" size={20}/>
+        <SearchButton onClick={handleSearch}>
+          <IoSearchSharp color="#636363" size={20} />
         </SearchButton>
       </TopBarDiv>
 
@@ -86,15 +91,25 @@ const TopBar = ({ onSearch, onToggle }) => {
 
       <ButtonWrapper>
         <FilterButton onClick={() => onToggle("parking")}>
-          <img src={Parking} alt="Parking"/>주차공간
+          <img src={Parking} alt="Parking" />
+          주차공간
         </FilterButton>
         <FilterButton onClick={() => onToggle("enforcement")}>
-          <img src={Warning} alt="enforcement"/>단속 구역
+          <img src={Warning} alt="enforcement" />
+          단속 구역
         </FilterButton>
-        <FilterButton>
-          <img src={Fire} alt="probability"/>단속확률
+        <FilterButton onClick={() => setOpenModal(true)}>
+          <img src={Fire} alt="probability" />
+          단속확률
         </FilterButton>
       </ButtonWrapper>
+
+      <CustomModal
+        isOpen={openModal}
+        onRequestClose={() => setOpenModal(false)}
+      >
+        <UploadModal />
+      </CustomModal>
     </>
   );
 };
@@ -161,9 +176,9 @@ const ButtonWrapper = styled.div`
   display: flex;
   position: absolute;
   padding: 5px 7px;
-  justify-content: center; 
-  gap: 15px; 
-  margin-top: 10px; 
+  justify-content: center;
+  gap: 15px;
+  margin-top: 10px;
   left: 50%;
   transform: translateX(-50%);
   width: 90%;
@@ -176,7 +191,7 @@ const FilterButton = styled.button`
   display: flex;
   background-color: #ffffff;
   border: none;
-  justify-content: center; 
+  justify-content: center;
   padding: 10px 0;
   border-radius: 20px;
   cursor: pointer;
