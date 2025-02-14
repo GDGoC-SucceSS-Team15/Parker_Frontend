69d5dac9c798798703eacc414f9bec355e82aca3
solux <lee.yeji0327@gmail.com>
2025-02-14 22:51:38 +0900

    feat: result가 error일 때 재업로드 화면 구현

--- src/components/Ai/UploadModal.jsx
+++ src/components/Ai/UploadModal.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { IoWarningOutline } from "react-icons/io5";
-import { CiImageOn } from "react-icons/ci";
+import warning from "../../assets/warning.svg";
+import imageIcon from "../../assets/image.svg";
+import uploadImg from "../../assets/uploadImg.svg";
 
 const UploadModal = () => {
+  const [result, setResult] = useState("");
+
+  const handlePostImage = () => {
+    setResult("error");
+  };
   return (
     <Wrapper>
       <div className="icon">
-        <IoWarningOutline size={50} color="#FF4D00" />
+        {result === "error" ? (
+          <img src={uploadImg} alt="uploadImg" />
+        ) : (
+          <img src={warning} alt="warning" />
+        )}
       </div>
       <Text>
-        <div className="title">
-          주정차 <span>상습 단속</span> 구역 여부
-        </div>
-        <div className="sub">
-          현재 위치가 상습 단속 구역인지 AI가 알려드립니다!
-        </div>
-        <div className="warn">
+        {result === "error" ? (
+          <>
+            <div className="title">
+              가이드에 맞게 촬영된 <br />
+              이미지를 업로드해주세요
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="title">
+              주정차 <span>상습 단속</span> 구역 여부
+            </div>
+            <div className="sub">
+              현재 위치가 상습 단속 구역인지 AI가 알려드립니다!
+            </div>
+          </>
+        )}
+        <div
+          className="warn"
+          style={{ color: result === "error" ? "#FF0000" : "black" }}
+        >
           * 차선이 보이게 찍어주세요. <br />* 주변 사물이 전체적으로 보이게
           찍어주세요.
         </div>
@@ -24,12 +48,12 @@ const UploadModal = () => {
           <input id="ai-image" type="file" />
           <label for="ai-image">
             <div className="icon">
-              <CiImageOn size={50} color="#B7B7B7" />
+              <img src={imageIcon} alt="imageIcon" />
             </div>
           </label>
         </Input>
-        <ConfirmBtn>확인하기</ConfirmBtn>
       </Text>
+      <ConfirmBtn onClick={handlePostImage}>확인하기</ConfirmBtn>
     </Wrapper>
   );
 };
