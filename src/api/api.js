fc52c0feab07afc542eb2644aa99ece8764bcce0
solux <lee.yeji0327@gmail.com>
2025-02-21 23:12:37 +0900

    feat: api 로딩 기능 구현

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { loadingStore } from "../store/loadingStore";
 
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_ROOT}`,
@@ -11,9 +12,25 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
+    const { startLoading } = loadingStore.getState(); // 로딩 처리
+    startLoading();
+
     return config;
   },
   (error) => Promise.reject(error)
 );
 
+api.interceptors.response.use(
+  (response) => {
+    const { stopLoading } = loadingStore.getState(); // Zustand에서 stopLoading 함수 가져오기
+    stopLoading(); // API 호출 성공 시 로딩 상태 감소
+    return response;
+  },
+  (error) => {
+    const { stopLoading } = loadingStore.getState(); // Zustand에서 stopLoading 함수 가져오기
+    stopLoading(); // API 호출 실패 시 로딩 상태 감소);
+    return Promise.reject(error);
+  }
+);
 export default api;
