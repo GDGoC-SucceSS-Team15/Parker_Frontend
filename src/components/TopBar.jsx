91904183ab5da70e97cd54ae5ec4d24aa7eb4ed3
Eunha Kim <rladmsgki@naver.com>
2025-02-26 23:39:54 +0900

    add: 필터링 버튼 기능 추가

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -16,6 +16,7 @@ const TopBar = ({ onSearch, onToggle }) => {
   const menuRef = useRef(null);
   const topBarRef = useRef(null);
   const [openModal, setOpenModal] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
   const [modal, setModal] = useState(false);
   const [result, setResult] = useState(null);
@@ -43,8 +44,11 @@ const TopBar = ({ onSearch, onToggle }) => {
     setIsMenuVisible((prev) => !prev);
   };
 
-  // 필터링
+  // 필터링 & 버튼 상태관리
   const handleClick = (filterType) => {
+      setSelectedFilters((prev) =>
+      prev.includes(filterType) ? prev.filter((f) => f !== filterType) : [...prev, filterType]
+      );
     onToggle(filterType);
   };
 
@@ -93,12 +97,18 @@ const TopBar = ({ onSearch, onToggle }) => {
       )}
 
       <ButtonWrapper>
-        <FilterButton onClick={() => handleClick("parking")}>
+        <FilterButton
+          onClick={() => handleClick("parking")}
+          selected={selectedFilters.includes("parking")}
+          >
           <img src={Parking} alt="parking" />
           {/* <LuSquareParking color="#015900" size={22} /> */}
           주차공간
         </FilterButton>
-        <FilterButton onClick={() => handleClick("crackdown")}>
+        <FilterButton
+          onClick={() => handleClick("crackdown")}
+          selected={selectedFilters.includes("crackdown")}
+        >
           <img src={Crackdown} alt="crackdown" />
           단속구역
         </FilterButton>
@@ -213,7 +223,7 @@ const ButtonWrapper = styled.div`
 
 const FilterButton = styled.button`
   display: flex;
-  background-color: #ffffff;
+  background-color: ${({ selected }) => (selected ? "#BDBDBD" : "white")};
   border: none;
   justify-content: center;
   padding: 10px 0;
